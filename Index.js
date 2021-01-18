a453a7489d06e19c49a304d055165d305dd31ef2
Roy Lee <royk.leejr@gmail.com>
2021-01-17 23:51:45 -0500

    changed start script

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const searchRoute = require("./routes/search.js");
 const nominationsRoute = require("./routes/nominations.js");
