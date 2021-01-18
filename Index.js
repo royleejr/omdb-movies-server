f82fbc60e606706071c87a49d08b0822e53d7f29
Roy Lee <royk.leejr@gmail.com>
2021-01-18 00:52:06 -0500

    tried commen ting out code

--- Index.js
+++ Index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000;
 // const searchRoute = require("./routes/search.js");
 // const nominationsRoute = require("./routes/nominations.js");
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 // app.use("/search", searchRoute);
 // app.use("/nominations", nominationsRoute);
