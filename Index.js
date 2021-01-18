d9bfd18665e25342c7c711a7a6c172ea7f9c0660
Roy Lee <royk.leejr@gmail.com>
2021-01-18 00:46:27 -0500

    seeing if this works

--- Index.js
+++ Index.js
@@ -3,14 +3,18 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-const searchRoute = require("./routes/search.js");
+// const searchRoute = require("./routes/search.js");
 // const nominationsRoute = require("./routes/nominations.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/search", searchRoute);
+// app.use("/search", searchRoute);
 // app.use("/nominations", nominationsRoute);
+
+app.get("/", (req, res) => {
+  res.send("It's working");
+});
 
 app.listen(PORT, () => {
   console.log("The server is running");
