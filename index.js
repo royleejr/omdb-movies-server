e1f25a71d8f8499305be295e8edced007ab0d872
Roy Lee <royk.leejr@gmail.com>
2021-01-18 01:05:53 -0500

    uncommented paths

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-// const searchRoute = require("./routes/search.js");
-// const nominationsRoute = require("./routes/nominations.js");
+const searchRoute = require("./routes/search.js");
+const nominationsRoute = require("./routes/nominations.js");
 
 app.use(cors());
 app.use(express.json());
