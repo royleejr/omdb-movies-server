b7153081a0e4086ac362bac9dad1f51ab0255455
Roy Lee <royk.leejr@gmail.com>
2021-01-18 00:35:17 -0500

    testing without search route

--- Index.js
+++ Index.js
@@ -3,13 +3,13 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-const searchRoute = require("./routes/search.js");
+// const searchRoute = require("./routes/search.js");
 const nominationsRoute = require("./routes/nominations.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/search", searchRoute);
+// app.use("/search", searchRoute);
 app.use("/nominations", nominationsRoute);
 
 app.listen(PORT, () => {
