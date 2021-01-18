184d8f535385a8613b7f271a12f77a5ed3266a6a
Roy Lee <royk.leejr@gmail.com>
2021-01-18 00:42:30 -0500

    checking

--- Index.js
+++ Index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 // const searchRoute = require("./routes/search.js");
-const nominationsRoute = require("./routes/nominations.js");
+// const nominationsRoute = require("./routes/nominations.js");
 
 app.use(cors());
 app.use(express.json());
 
 // app.use("/search", searchRoute);
-app.use("/nominations", nominationsRoute);
+// app.use("/nominations", nominationsRoute);
 
 app.listen(PORT, () => {
   console.log("The server is running");
